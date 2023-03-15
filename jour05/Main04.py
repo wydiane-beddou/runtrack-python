68f977721ef00a13b28169327bb483e94efec9cd
Weezy13090 <wydiane.beddou@laplateforme.io>
2023-03-15 17:13:11 +0100

    cesar

--- jour05/Main04.py
+++ jour05/Main04.py
@@ -1,5 +1,25 @@
-def cesar():
-    import string 
-    text="le soleil va disparaitre"
-    
+import string 
 
+def cesar(): # fonction
+    text="le soleil va disparaitre" # chaine de texte
+    alphabet = string.ascii_lowercase + string.ascii_uppercase
+    #chaîne d'alphabet en combinant les lettres minuscules et majuscules 
+    # en utilisant les constantes fournies par le module "string"
+    shift = 3 # décale de 3 lettres dans l'alphabet
+
+    shifted = alphabet[shift:] + alphabet[:shift] 
+    #Cette variable contient l'alphabet original décalé de 3 positions 
+    # vers la droite, suivi des lettres qui ont été découpées 
+    # à partir du début de l'alphabet original
+    
+    table = str.maketrans(alphabet, shifted) 
+    # Cette variable contient l'alphabet original décalé de 3 positions vers la droite, 
+    # suivi des lettres qui ont été découpées à partir du début de l'alphabet original
+
+    cesar = text.translate(table)
+    # La variable "cesar" contient le message chiffré. La méthode "translate" est appelée 
+    # sur la chaîne "text" en utilisant la table de traduction créée précédemment
+
+    print(cesar)
+
+cesar()
